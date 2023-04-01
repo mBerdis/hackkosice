5a32d689f095d2d2a12711b4f4861b6041289a33
mBerdis <marosberdis@gmail.com>
2023-04-01 20:25:50 +0200

    update

--- test.py
+++ test.py
@@ -36,10 +36,10 @@ class MyMapView(MapView):
 
     def do_update(self, dt):  # this over-rides the do_update() method of MapView
         super(MyMapView, self).do_update(dt)
-        self.draw_lines()
+        self.draw_circle()
 
-    # draw the lines
-    def draw_lines(self):
+    # draw the circle
+    def draw_circle(self):
 # Define the radius of the Earth in kilometers
         R = 6371.01
 
@@ -52,28 +52,26 @@ class MyMapView(MapView):
 
         # Calculate the distance from the center point to a point 1 kilometer to the north, south, east, and west
         d = 2  # 2x1 kilometer
-        lat_north = math.asin(math.sin(lat1) * math.cos(d/R) + math.cos(lat1) * math.sin(d/R) * math.cos(0))
-        lon_north = lon1 + math.atan2(math.sin(0) * math.sin(d/R) * math.cos(lat1), math.cos(d/R) - math.sin(lat1) * math.sin(lat_north))
-        lat_north = math.degrees(lat_north)
-        lon_north = math.degrees(lon_north)
+        lat_north = math.degrees(math.asin(math.sin(lat1) * math.cos(d/R) + math.cos(lat1) * math.sin(d/R) * math.cos(0)))
+        lon_north = math.degrees(lon1 + math.atan2(math.sin(0) * math.sin(d/R) * math.cos(lat1), math.cos(d/R) - math.sin(lat1) * math.sin(lat_north)))
 
         marker = MapMarker(lat = lat_north, lon = lon_north)
         self.add_marker(marker)
         radius = marker.y - self.m1.y
         self.remove_marker(marker)
 
-        lines = Ellipse(pos = (self.m1.center_x - radius/2, self.m1.center_y - radius/2), size = (radius, radius))
+        circle = Ellipse(pos = (self.m1.center_x - radius/2, self.m1.center_y - radius/2), size = (radius, radius))
 
         if self.grp is not None:
-            # just update the group with updated lines lines
+            # just update the group with updated circle circle
             self.grp.clear()
-            self.grp.add(lines)
+            self.grp.add(circle)
         else:
             with self.canvas.after:
-                #  create the group and add the lines
+                #  create the group and add the circle
                 Color(1,0,0,0.4)  # line color
                 self.grp = InstructionGroup()
-                self.grp.add(lines)
+                self.grp.add(circle)
 
 
 class MapViewApp(App):
